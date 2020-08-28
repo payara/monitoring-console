af28ec1cae7a5c61455d6e9fde7c86c0a22cd4af
Jan Bernitt <jaanbernitt@gmail.com>
2020-08-28 12:22:38 +0200

    FISH-357 adds page # of columns setting

--- webapp/src/main/webapp/js/mc-view.js
+++ webapp/src/main/webapp/js/mc-view.js
@@ -394,8 +394,9 @@ MonitoringConsole.View = (function() {
                 { available: renameAvailable, input: [ name, rename ] },
                 { available: !renameAvailable, input: $('<span/>').text(Page.name()) },
             ]},
-            { label: 'Include in Rotation', type: 'toggle', options: { false: 'No', true: 'Yes' }, value: Page.rotate(), onChange: (checked) => Page.rotate(checked) },
             { label: 'Type', type: 'dropdown', options: {manual: 'Manual', query: 'Query'}, value: page.type, onChange: (type) => { onPageUpdate(configure(page => page.type = type)); updateSettings(); } },            
+            { label: 'Number of Columns', type: 'range', min: 1, max: 8, value: page.numberOfColumns, onChange: columns =>  { onPageUpdate(Page.arrange(columns)); updatePageNavigation(); }},
+            { label: 'Include in Rotation', type: 'toggle', options: { false: 'No', true: 'Yes' }, value: Page.rotate(), onChange: (checked) => Page.rotate(checked) },
             { label: 'Max Size', available: queryAvailable, type: 'value', min: 1, unit: 'count', value: page.content.maxSize,  onChange: (value) => configure(page => page.content.maxSize = value) },
             { label: 'Query Series', available: queryAvailable, type: 'text', value: page.content.series, onChange: (value) => configure(page => page.content.series = value) },
             { label: 'Query Interval', available: queryAvailable, input: [
